ace9626ff48e0e4e354ae059f228bf489a7f71f8
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-08-01 16:57:10 +0100

    final commit, not much closer to solution, time is up

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,8 +5,8 @@
 <div class="bg-white p-4 w-2/3 mx-auto">
     <h2 class="text-center text-2xl">Upload your dog!</h2>
     <p>Please add your form code here</p>
-    <!-- Below form added as a "template" to work with, seems like it allwws grabbing file from disk but getting error:  "The POST method is not supported for this route. Supported methods: GET, HEAD, PUT, PATCH, DELETE." -->
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <!-- @Piotr - Below form added as a "template" to work with, seems like it allwws grabbing file from disk but getting error:  "The POST method is not supported for this route. Supported methods: GET, HEAD, PUT, PATCH, DELETE." Have to update something in PictureController I guess... the " store(Request $request)" part-->
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
   Select image to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">  
